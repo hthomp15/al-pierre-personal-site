365c0784d7352e06e2bb67cbcb9a0fd9a751958e
Haile Thompson <hailethomp@gmail.com>
2023-02-05 18:36:10 -0500

    [add] about page content. Links to home and /about

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,18 @@
-
+import Link from 'next/link';
 
 export default function Header() {
     return (
         <nav className="flex justify-evenly items-center">
             <div className="flex-col items-baseline">
-                <h1 className="text-5xl">Alphonse Pierre</h1>
+                <Link href="/">
+                    <h1 className="text-5xl">Alphonse Pierre</h1>
+                </Link>
                 <p>Writer | Pitchfork</p>
             </div>
             <ul className="inline-flex list-none"> 
-                <li className="px-2">About</li>
+                <Link href="/about">
+                    <li className="px-2">About</li>
+                </Link>
                 <li>Writing</li>
             </ul>
         </nav>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
 import '@/styles/tailwind.css'
+import Header from '@/components/Header'
 import type { AppProps } from 'next/app'
+import { useEffect, useRef } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }: AppProps) {
+
+  function usePrevious(value: any) {
+    let ref = useRef()
+  
+    useEffect(() => {
+      ref.current = value
+    }, [value])
+  
+    return ref.current
+  }
+
+  let previousPathname = usePrevious(router.pathname)
+  return (
+    <div>
+      <Header />
+      <main>
+        <Component previousPathname={previousPathname} {...pageProps} />  
+      </main>
+    </div>
+  )
 }
