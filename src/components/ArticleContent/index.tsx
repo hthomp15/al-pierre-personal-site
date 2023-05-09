781d95bb554c15b43e848b0ebfc1c8ae65eae079
Haile Thompson <hailethomp@gmail.com>
2023-05-08 20:47:23 -0400

    add routing based on pathname

--- src/components/ArticleContent/index.tsx
+++ src/components/ArticleContent/index.tsx
@@ -1,4 +1,4 @@
-
+import { usePathname } from 'next/navigation';
 
 
 const featuredPost = {
@@ -17,7 +17,7 @@ const featuredPost = {
     },
     
 }
-const posts = [
+const reviews = [
     {
         id: 2,
         title: 'Boost your conversion rate',
@@ -77,10 +77,73 @@ const posts = [
                 'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         },
     },
-
 ]
 
+const ScenePieces = [
+    {
+        id: 2,
+        title: 'Boost your conversion rate',
+        href: '#',
+        description:
+            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+        date: 'Mar 10, 2020',
+        datetime: '2020-03-16',
+        author: {
+            name: 'Lindsay Walton',
+            href: '#',
+            imageUrl:
+                'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+    },
+    {
+        id: 3,
+        title: 'Boost your conversion rate',
+        href: '#',
+        description:
+            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+        date: 'Mar 10, 2020',
+        datetime: '2020-03-16',
+        author: {
+            name: 'Lindsay Walton',
+            href: '#',
+            imageUrl:
+                'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+    },
+    {
+        id: 4,
+        title: 'Boost your conversion rate',
+        href: '#',
+        description:
+            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+        date: 'Mar 10, 2020',
+        datetime: '2020-03-16',
+        author: {
+            name: 'Lindsay Walton',
+            href: '#',
+            imageUrl:
+                'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+    },
+    {
+        title: 'Boost your conversion rate',
+        href: '#',
+        description:
+            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+        date: 'Mar 10, 2020',
+        datetime: '2020-03-16',
+        author: {
+            name: 'Lindsay Walton',
+            href: '#',
+            imageUrl:
+                'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+    },
+]
+
+
 export default function ArticleContent() {
+    const currentPath = usePathname();
     return (
         <div className="bg-white py-12 sm:py-12">
             <div className="mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-12 px-6 sm:gap-y-16 lg:grid-cols-2 lg:px-8">
@@ -113,10 +176,9 @@ export default function ArticleContent() {
                         </div>
                     </div>
                 </article>
-                
                 <div className="mx-auto w-full max-w-2xl border-t border-gray-900/10 pt-12 sm:pt-16 lg:mx-0 lg:max-w-none lg:border-t-0 lg:pt-0">
                     <div className="-my-12 divide-y divide-gray-900/10">
-                        {posts.map((post) => (
+                        { currentPath === "/reviews" ? reviews.map((post) => (
                             <article key={post.id} className="py-12">
                                 <div className="group relative max-w-xl">
                                     <time dateTime={post.datetime} className="block text-sm leading-6 text-gray-600">
@@ -140,7 +202,34 @@ export default function ArticleContent() {
                                     </a>
                                 </div>
                             </article>
-                        ))}
+                        ))
+                        : ScenePieces.map((post) => (
+                            <article key={post.id} className="py-12">
+                            <div className="group relative max-w-xl">
+                                <time dateTime={post.datetime} className="block text-sm leading-6 text-gray-600">
+                                    {post.date}
+                                </time>
+                                <h2 className="mt-2 text-lg font-semibold text-gray-900 group-hover:text-gray-600">
+                                    <a href={post.href}>
+                                        <span className="absolute inset-0" />
+                                        {post.title}
+                                    </a>
+                                </h2>
+                                <p className="mt-4 text-sm leading-6 text-gray-600">{post.description}</p>
+                            </div>
+                            <div className="mt-4 flex">
+                                <a
+                                    href={post.author.href}
+                                    className="relative flex gap-x-2.5 text-sm font-semibold leading-6 text-gray-900"
+                                >
+                                    <img src={post.author.imageUrl} alt="" className="h-6 w-6 flex-none rounded-full bg-gray-50" />
+                                    {post.author.name}
+                                </a>
+                            </div>
+                        </article>
+                        ))
+
+                            }
                     </div>
                 </div>
             </div>
