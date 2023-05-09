e5a535793682215fd48ad35e200ff3ad349bd768
Haile Thompson <hailethomp@gmail.com>
2023-05-08 20:53:27 -0400

    [add] remaining pages for each writing category

--- src/pages/film-and-tv.tsx
+++ src/pages/film-and-tv.tsx
@@ -1,17 +1,17 @@
 import Head from "next/head";
 import ArticleContent from "@/components/ArticleContent";
 
-export default function Reviews() {
+export default function FilmAndTv() {
     return (
         <>
             <Head>
-                <title>Alphonse Pierre - Reviews</title>
+                <title>Alphonse Pierre - Film & TV</title>
                 <meta name="description" content="Album reviews by Alphonse Pierre" />
             </Head>
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="mx-auto max-w-4xl mt-10">
                     <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-                        Reviews
+                        Film & TV
                     </h1>
                     <ArticleContent />
                 </div>
--- src/pages/scene-pieces.tsx
+++ src/pages/scene-pieces.tsx
@@ -5,13 +5,13 @@ export default function ScenePieces() {
 return (
     <>
         <Head>
-            <title>Alphonse Pierre - Reviews</title>
+            <title>Alphonse Pierre - Scene Pieces</title>
             <meta name="description" content="Album reviews by Alphonse Pierre"/>
         </Head>
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="mx-auto max-w-4xl mt-10">
                 <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-                    Reviews
+                    Scene Pieces
                 </h1>
                 <ArticleContent />
             </div>
