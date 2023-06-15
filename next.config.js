a6538d879ab2bccfa899e02d5c2208d8be333928
Haile Thompson <hailethomp@gmail.com>
2023-06-15 11:18:16 -0400

    fix: base path

--- next.config.js
+++ next.config.js
@@ -4,5 +4,8 @@ module.exports = {
   reactStrictMode: true,
   basePath: '/al-pierre-personal-site',
   assetPrefix: '/al-pierre-personal-site/',
+  publicRuntimeConfig: {
+    basePath: process.env.BASE_PATH || 'al-pierre-personal-site',
+  },
 }
 
